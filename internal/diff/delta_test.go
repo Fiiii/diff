3f8dff63083a7303d10d95fc97deaae46040706a
Artur Figiel <arturffigiel@gmail.com>
2024-03-04 16:27:29 +0100

    Add project structure with unit test

---
+++ internal/diff/delta_test.go
@@ -0,0 +1,18 @@
+package diff
+
+import (
+	"testing"
+)
+
+func TestDelta(t *testing.T) {
+	t.Log("Given the need to work with chunks division")
+	{
+		t.Error("Test not implemented")
+
+	}
+
+	t.Log("Given the need to work with delta calculations")
+	{
+		t.Error("Test not implemented")
+	}
+}
