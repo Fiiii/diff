240cbf14cfca9bf2fa7a4763450a560dc7ee1668
Artur Figiel <arturffigiel@gmail.com>
2024-03-04 17:51:58 +0100

    Create test loop for delta calculations

--- internal/diff/delta.go
+++ internal/diff/delta.go
@@ -1,1 +1,40 @@
+// Package diff provides a way to generate a delta between two byte slices.
 package diff
+
+type Delta struct {
+	ChunksToReuse []Chunk
+	Changes       []Change
+	Removals      []Chunk
+	Additions     []Chunk
+}
+
+type Change struct {
+	Position int
+	OldData  []byte
+	NewData  []byte
+}
+
+type Chunk struct {
+	Position int
+	Data     []byte
+}
+
+// GenerateDelta generates a delta between the original and updated byte slices.
+func GenerateDelta() (Delta, error) {
+	return Delta{}, nil
+}
+
+// divideIntoChunks divides the data into chunks of size chunkSize.
+func divideIntoChunks(chunkSize int, data []byte) []Chunk {
+	return []Chunk{}
+}
+
+// calculateRollingHashes calculates the rolling hashes for each chunk.
+func calculateRollingHashes(chunks []Chunk) []uint64 {
+	return []uint64{}
+}
+
+// compareHashes compares the hashes of the original and updated chunks to generate a delta.
+func compareHashes() Delta {
+	return Delta{}
+}
--- internal/diff/delta_test.go
+++ internal/diff/delta_test.go
@@ -1,18 +1,62 @@
 package diff
 
 import (
+	"reflect"
 	"testing"
+)
+
+const (
+	Success       = "\u2713"
+	Failed        = "\u2717"
+	testChunkSize = 3
 )
 
 func TestDelta(t *testing.T) {
 	t.Log("Given the need to work with chunks division")
 	{
-		t.Error("Test not implemented")
+		testID := 0
+		t.Logf("\tTest %d:\tWhen calculating chunks hashes.", testID)
+		{
+			originalData := []byte("hello")
+			expHashes := []uint64{0}
+			chunks := divideIntoChunks(testChunkSize, originalData)
+			hashes := calculateRollingHashes(chunks)
 
+			if reflect.DeepEqual(hashes, expHashes) {
+				t.Logf("\t%s\tTest %d:\tShould be able to return proper divided chunks.", Success, testID)
+			} else {
+				t.Logf("got: %v", hashes)
+				t.Logf("exp: %v", expHashes)
+				t.Fatalf("\t%s\tTest %d:\tShould be able to return proper divided chunks.", Failed, testID)
+			}
+		}
 	}
 
 	t.Log("Given the need to work with delta calculations")
 	{
-		t.Error("Test not implemented")
+		var deltaTestsCases = []struct {
+			originalData []byte
+			updatedData  []byte
+			delta        Delta
+		}{
+			{originalData: []byte("abc"), updatedData: []byte("abc"), delta: Delta{
+				ChunksToReuse: []Chunk{{0, []byte("abc")}}}},
+			{originalData: []byte("abc"), updatedData: []byte("abcd"), delta: Delta{
+				ChunksToReuse: []Chunk{{0, []byte("abc")}},
+				Additions:     []Chunk{{3, []byte("d")}}}},
+			{originalData: []byte("abcd"), updatedData: []byte("abc"), delta: Delta{
+				ChunksToReuse: []Chunk{{0, []byte("abc")}},
+				Removals:      []Chunk{{3, []byte("d")}}}},
+			{originalData: []byte("abc"), updatedData: []byte("def"), delta: Delta{
+				Changes: []Change{{0, []byte("abc"), []byte("def")}}}},
+		}
+		for i, tc := range deltaTestsCases {
+			testID := i + 1
+			t.Logf("\tTest %d:\tWhen handling delta calculation for original: `%s` and updated: `%s`.",
+				testID, tc.originalData, tc.updatedData)
+			{
+
+			}
+		}
 	}
 }
